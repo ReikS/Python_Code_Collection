1dfcdd97accbff28a6dec6bf75d0ec8ecc639d45
ReikS <Reik.Schottstedt@gmail.com>
2023-10-30 22:32:57 +0100

    added output to program header

---
+++ Python_Code_Collection.py
@@ -0,0 +1,37 @@
+################################################################
+# PROGRAM NAME : Python_Code_Collection.py
+# DESCRIPTION : Collection of useful python functions, code snippets, etc.
+#
+# AUTHOR : ReikS
+# CREATION DATE : 2023-19-25
+# LAST CHANGE DATE : 2023-19-25
+# REVIEWWER : <name of the reviewer>
+# REVIEW DATE : <date of the review yyyy-mm-dd>
+#	
+#	
+# SUMMARY : See below a list of the functions and code snippets contained
+#           in this collection : 
+#               000. Program header template
+#
+#
+# REVIEW SUMMARY : <reviewer's notes>
+# 
+#
+# INPUT : none
+# 
+# 
+# OUTPUT : none
+#
+#
+################################################################
+# CHANGE TRACKER
+# DATE			AUTHOR				DESCRIPTION
+# <yyyy-mm-dd>	<name of author>	<short description>
+#
+################################################################
+
+
+################################################################
+# 000. Program header template
+################################################################
+
